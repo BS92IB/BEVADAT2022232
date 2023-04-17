64efadda1b768391af4d73b8d56e90bbf7cca7cd
simocsongor.z@stud.uni-obuda.hu <simocsongor.z@stud.uni-obuda.hu>
2023-04-17 23:31:25 +0200

    Hazi06 done

--- HAZI/HAZI06/HAZI06.py
+++ HAZI/HAZI06/HAZI06.py
@@ -51,3 +51,49 @@ HAZI-
 ##                                                              ##
 ##################################################################
 """
+from sklearn.model_selection import train_test_split
+from NJCleaner import NJCleaner
+from DecisionTreeClassifier  import DecisionTreeClassifier
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+
+nj = NJCleaner('./2018_03.csv')
+nj.prep_df()
+
+
+col_name = ['stop_sequence' , 'from_id' , 'to_id' , 'status' , 'line' , 'type' , 'day', 'part_of_the_day' , 'delay']
+data = pd.read_csv('./data/NJ.csv',skiprows=1, header=None, names=col_name)
+
+
+X = data.iloc[:, :-1].values
+Y = data.iloc[:, -1].values.reshape(-1,1)
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=.2, random_state=41)
+
+
+classifier = DecisionTreeClassifier(min_samples_split=2, max_depth=4)
+classifier.fit(X_train, Y_train)
+
+Y_pred = classifier.predict(X_test)
+print(accuracy_score(Y_test, Y_pred))
+
+
+# min_samples_split=2, max_depth=4
+#1  min_samples_split=1, max_depth=1 --> accuracy: 0.7773333333333333
+#2  min_samples_split=1, max_depth=2 --> accuracy: 0.7823333333333333
+#3  min_samples_split=1, max_depth=3 --> accuracy: 0.7839166666666667
+#4  min_samples_split=1, max_depth=4 --> accuracy: 0.7849166666666667
+#5  min_samples_split=2, max_depth=1 --> accuracy: 0.7773333333333333
+#6  min_samples_split=2, max_depth=2 --> accuracy: 0.7823333333333333
+#7  min_samples_split=2, max_depth=3 --> accuracy: 0.7839166666666667
+#8  min_samples_split=2, max_depth=4 --> accuracy: 0.7849166666666667
+#9  min_samples_split=3, max_depth=1 --> accuracy: 0.7773333333333333
+#10 min_samples_split=3, max_depth=2 --> accuracy: 0.7823333333333333
+
+#Néhány split-depth pár amikkel 78% körüli accuracy érhető el.
+#Észrevétel: A split értéke nem befolyásolja az eredményt.
+#A legpontosabb eredmény a depth növelésével érhető el.
+#A depth növelésékor errorokra futottam.
+#A depth növelésével egyre tovább tartott lefutnia a modelnek.
+
+#Először kicsit bonyolultnak tűnt a feladat, de jobban belegondolva miután megértettem jobb volt mint az eddigi házik.
